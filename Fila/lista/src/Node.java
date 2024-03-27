97309b3c8612d266d406a8beb20d6b1c1aa2c094
Paulo Victor Frazão <128742259+Pvictorfrazao@users.noreply.github.com>
2024-03-27 00:25:42 -0300

    Add files via upload

---
+++ Fila/lista/src/Node.java
@@ -0,0 +1,16 @@
+public class Node {
+
+    int data;
+    Node prev; //Referência ao nó anterior
+    Node next; //Referência ao nó posterior
+
+    public Node(int data){
+        this.data = data;
+        this.prev = null;
+        this.next = null;
+    }
+
+    
+
+
+}
