4f923f67b19878c13b1abe3ace4b08bd350b9c1e
Paulo Victor Frazão <128742259+Pvictorfrazao@users.noreply.github.com>
2024-03-27 00:30:42 -0300

    Add files via upload

---
+++ Pilha/src/Node.java
@@ -0,0 +1,14 @@
+public class Node {
+
+    int data;
+    Node next; //Referência ao nó posterior
+
+    public Node(int data){
+        this.data = data;
+        this.next = null;
+    }
+
+    
+
+
+}
